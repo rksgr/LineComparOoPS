91513f37a5edca06d9bbdb0c4221bafca9b72be1
rksgr <r_kjha@yahoo.com>
2021-12-14 09:40:24 +0530

    Added: Welcome message

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+package com.mycompany.dec_13_linecompar_oops;
+
+public class Welcome {
+    public static void main(String []args){
+       System.out.println("Welcome to Line Comparison Computation Program on "
+            + "Master Branch!");
+    }
+}
+
