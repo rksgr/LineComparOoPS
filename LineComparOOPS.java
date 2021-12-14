fb4e670ca26f65cf452dbf48df59d90b33e82d87
rksgr <r_kjha@yahoo.com>
2021-12-14 10:44:43 +0530

    Refactored: compare two lines

--- LineComparOOPS.java
+++ LineComparOOPS.java
@@ -25,14 +25,29 @@ public class LineComparOOPS {
         return lines_eq;
     }
     /*
+    Use case 3: Compare two lines
+    */
+    public String compareLines(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4){
+        // Call method which calculates length of lines
+        double len_line1 = lineLength(x1,y1,x2,y2);
+        double len_line2 = lineLength(x3,y3,x4,y4);
+        String lin_comp = "";
+        if (len_line1>len_line2){
+            lin_comp = "Line 1 is greater in length";
+        } else if (len_line1<len_line2){
+            lin_comp = "Line 2 is greater in length";
+        }else if (len_line1==len_line2){
+            lin_comp = "Both lines are equal in length";
+        }
+        return lin_comp;
+    }
+    /*
     Main method
     */
     public static void main(String []args){
         LineComparOOPS lco = new LineComparOOPS();
-        // check equality of two lines
-        boolean lines_equal = lco.checkLinesEquality(0,0,1,1,0,2,3,3);
-        if (lines_equal){
-            System.out.println("The lines are equal");
-        } else{ System.out.println("The lines are not equal");}
+        // compare two lines
+        String lines_comp = lco.compareLines(0,0,1,1,0,2,3,3);
+        System.out.println(lines_comp);
     }
 }
